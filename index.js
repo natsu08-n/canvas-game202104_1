1d6c5cc2e48cd77afe72b41f1aca80e2e04e36e8
natsu08-n <merrysheep18@gmail.com>
2021-04-17 16:34:32 +0900

    ユニコーン生成と移動機能修正、CSS修正

--- index.js
+++ index.js
@@ -1,70 +1,83 @@
 const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    let currentSelected = null;
-    let mode = 'create';
-    let characters = [];
+const ctx = canvas.getContext('2d');
+let currentSelected = null;
+let mode = '';
+let characters = [];
 
-    class Character {
-      constructor(x, y, src, id) {
-        this.x = x;
-        this.y = y;
-        this.src = src;
-        this.id = id;
-        this.image = new Image();
-        this.image.src = this.src;
-        this.image.id = this.id;
-        this.image.onload = () => this.draw();
-        characters.push(this);
-      }
-      draw() {
-        ctx.drawImage(this.image, this.x, this.y, 200, 200);
-      }
-      clicked(point) {
-        if(point.x > this.x && this.x + 200 > point.x ) {
-          console.log('x座標' + this.id);
-          // elmObject = this.id;
-          currentSelected = this;
-        } 
-      }
+const createBtn = document.getElementById('js-create');
+const moveBtn = document.getElementById('js-move');
+
+createBtn.addEventListener("click", (e) => {
+  e.preventDefault();//「クリックしたとき」のデフォルト挙動キャンセル
+  window.onkeydown = null;
+  mode = 'create';
+});
+
+moveBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  // モードがmoveの時だけonkeydown使うのでこちらに移動
+  window.onkeydown = (e) => {
+    if(currentSelected === null) {
+      return
     }
+    ctx.clearRect(0, 0, 800, 800);
+    if (e.code === "ArrowRight") {
+      currentSelected.x += 20;
+    } else if (e.code === "ArrowLeft") {
+      currentSelected.x -= 20;
+    } else if (e.code === "ArrowUp") {
+      currentSelected.y += 20;
+    } else if (e.code === "ArrowDown") {
+      currentSelected.y -= 20;
+    }
+  }
+  mode = 'move'
+});
 
-    canvas.addEventListener("click", e => {
-      const rect = canvas.getBoundingClientRect();
-      const point = {
-        x: e.clientX,
-        y: e.clientY
-      }
-      if(mode !== 'create') {
-        characters.forEach(chara => {
-          chara.clicked(point);
-        });
-      } else if(mode === 'create'){
-        new Character(point.x - 100, point.y - 100, './business_unicorn_company.png', 'chara_1');
-      }
+class Character {
+  constructor(x, y, src, id) {
+    this.x = x;
+    this.y = y;
+    this.src = src;
+    this.id = id;
+    this.image = new Image();
+    this.image.src = this.src;
+    this.image.id = this.id;
+    this.image.onload = () => this.draw();
+    characters.push(this);
+  }
+  draw() {
+    ctx.drawImage(this.image, this.x, this.y, 200, 200);
+  }
+  clicked(point) {
+    if((point.x > this.x && this.x + 200 > point.x) &&(point.y > this.y && this.y + 200 > point.y)) {
+      console.log('xy座標' + this.id);
+      // elmObject = this.id;
+      currentSelected = this;
+    } 
+  }
+}
+
+canvas.addEventListener("click", e => {
+  const rect = canvas.getBoundingClientRect();
+  const point = {
+    x: e.clientX,
+    y: e.clientY
+  }
+  if(mode !== 'create') {
+    currentSelected = null;
+    characters.forEach(chara => {
+      chara.clicked(point);
     });
+  } else if(mode === 'create'){
+    new Character(point.x - 100, point.y - 100, './business_unicorn_company.png', 'chara_1');
+  }
+});
 
-    window.onkeydown = (e) => {
-      ctx.clearRect(0, 0, 800, 800);
-      if (e.code === "ArrowRight") {
-        currentSelected.x += 20;
-      } else if (e.code === "ArrowLeft") {
-        currentSelected.x -= 20;
-      } else if (e.code === "ArrowUp") {
-        currentSelected.y += 20;
-      } else if (e.code === "ArrowDown") {
-        currentSelected.y -= 20;
-      }
-      // currentSelected.draw();
-    }
-
-    function mainloop() {
-      characters.forEach(chara => {
-          chara.draw();
-        });
-      // chara1.draw();
-      // chara2.draw();
-      // chara3.draw();
-      requestAnimationFrame(mainloop);
-    }
-    mainloop();
-
+function mainloop() {
+  characters.forEach(chara => {
+      chara.draw();
+    });
+  requestAnimationFrame(mainloop);
+}
+mainloop();
