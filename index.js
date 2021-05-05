c153e3ac62ac2cc7407ab76caec3d0b90c19ac66
natsu08-n <merrysheep18@gmail.com>
2021-05-05 12:19:40 +0900

    制限時間追加、敵をランダムに上から降らす処理追加

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let currentSelected = null;
 let mode = '';
 let characters = [];
 let enemies = [];
+// 制限時間について
+const timerId = document.getElementById('timer');
+let LimitTime;
 
 const createBtn = document.getElementById('js-create');
 const moveBtn = document.getElementById('js-move');
@@ -54,6 +57,7 @@ class Character extends GameObject {
   constructor(x, y, src, id) {
     super(x, y, src, id)
     this.speed = 20;
+    this.hit = false;
     characters.push(this);
   }
   clicked(point) {
@@ -76,6 +80,25 @@ class Character extends GameObject {
     // centerX,Yを再計算
     this.centerX = this.x + this.width / 2;
     this.centerY = this.y + this.height / 2;
+    this.collision();
+  }
+  collision() {
+    this.hit = false;
+    enemies.forEach(enemy => {
+      let distance = this.calcDistance(enemy);
+      if (distance <= 30) {
+        this.hit = true;
+        // console.log("敵と遭遇しました");
+        // console.log("chara.hit: " + chara.hit);
+      }
+    });
+  }
+  // 距離を計算するメソッド
+  calcDistance(enemy) {
+    //敵との当たり判定（2点間の中心座標の距離）
+    let sqrt = Math.pow(this.x - enemy.x, 2) + Math.pow(this.y - enemy.y, 2);
+    let distance = Math.sqrt(sqrt);
+    return distance;
   }
 }
 
@@ -83,30 +106,16 @@ class Character extends GameObject {
 class Enemy extends GameObject {
   constructor(x, y, src, id) {
     super(x, y, src, id)
+    this.speed = Math.random() * 5 + 2;
   }
   move() {
     this.x += 0;
-    this.y += .5;
+    this.y += this.speed;
     this.centerX = this.x + this.width / 2;
     this.centerY = this.y + this.height / 2;
-    this.collision();
+    // this.collision();
   }
-  collision() {
-    characters.forEach(chara => {
-      let distance = this.calcDistance(chara);
-      if(distance <= 30) {
-        console.log("敵と遭遇しました");
-          canvas.classList.add('atari');
-        }
-    });
-  }
-  // 距離を計算するメソッド
-  calcDistance(chara) {
-    //敵との当たり判定（2点間の中心座標の距離）
-    let sqrt = Math.pow(this.x - chara.x, 2) + Math.pow(this.y - chara.y, 2);
-    let distance = Math.sqrt(sqrt);
-    return distance;
-  }
+  
 }
 
 canvas.addEventListener("click", e => {
@@ -128,28 +137,61 @@ canvas.addEventListener("click", e => {
 
 // 敵の処理
 function generateEnemy() {
-  for(let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
     let enemy = new Enemy(110 * i, 0, './img/fantasy_orc.png', 'enemy_1');
     enemies.push(enemy);
   }
 }
 
-setInterval(generateEnemy, 5000);
+setInterval(generateEnemy, 3000);
 
 function mainloop() {
+  let flg = false;
   ctx.clearRect(0, 0, 800, 800);
   // ユニコーンの処理
   characters.forEach(chara => {
     chara.draw();
+    if(chara.hit) {
+      flg = true;
+    }
   });
+  // console.log(flg);
 
-  // console.log(enemyImg);
   enemies.forEach(enemy => {
     enemy.move();
     enemy.draw();
-
   })
+
+  if(flg) {
+    canvas.classList.add('atari');
+  } else {
+    canvas.classList.remove('atari');
+  }
 
   requestAnimationFrame(mainloop);
 }
 mainloop();
+
+function init() {
+  LimitTime = 20;
+  timerId.innerHTML = LimitTime;
+}
+
+function updateTime() {
+  setTimeout(function () {
+    LimitTime--;
+    timerId.innerHTML = LimitTime;
+    if (LimitTime < 0) {
+      // alert('Game Over!');
+      init();
+      return;
+    }
+    updateTime();
+  }, 1000);
+}
+
+window.onload = function () {
+  init();
+  updateTime();
+}
+
